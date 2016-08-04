6065111819642b3cd4e8082c843501485fe0da17
Super Snake King <skteh@appsploration.com>
2016-08-04 17:29:17 +0800

    Add posts creation
    Add posts creation.

---
+++ database/migrations/2016_08_04_090117_create_posts.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePosts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('post_types_id');
+            $table->string('brand', 100);
+            $table->string('title', 64);
+            $table->text('desc', 255);
+            $table->enum('date_type', ['s_n', 'wsl', 'end', 'none'])->default('s_n');
+            $table->date('start');
+            $table->date('end');
+            $table->date('publish_start');
+            $table->date('publish_end');
+            $table->string('product_url', 255);
+            $table->text('keywords');
+            $table->enum('approval_status', ['pending', 'publish', 'unpublish']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
