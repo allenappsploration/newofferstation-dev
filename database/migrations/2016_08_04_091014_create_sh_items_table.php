c30a43ffd5feca65cca103063401aaf891f5611e
Allen <allen@appsploration.com>
2016-08-04 17:45:59 +0800

    Added sh_items creation
    Added sh_items table creation

--- database/migrations/2016_08_04_091014_create_sh_items_table.php
+++ database/migrations/2016_08_04_091014_create_sh_items_table.php
@@ -12,7 +12,29 @@ class CreateShItemsTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('sh_items', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->integer('nid')->unsigned()->default(0)->comment('socialhub post id');
+            $table->integer('tid')->unsigned()->default(0)->comment('socialhub brand id');
+            $table->string('name', 255)->comment('socialhub brand name');
+            $table->string('author_name', 255);
+            $table->text('raw_body');
+            $table->string('social_channel', 255);
+            $table->string('post_type', 255)->comment('socialhub post type');
+            $table->string('url', 255)->comment('social media source url');
+            $table->integer('status')->default(1);
+            $table->timestamp('created');
+            $table->string('img', 255)->nullable();
+            $table->integer('img_width')->unsigned()->default(0);
+            $table->integer('img_height')->unsigned()->default(0);
+            $table->json('extra_data')->nullable()->comment('dmp details in json format');
+            $table->text('tag')->nullable()->comment('tagging from socialhub');
+            $table->string('post_language', 50)->nullable();
+            $table->boolean('is_linked')->default(0);
+            $table->timestamps();
+            
+            $table->index(['nid', 'tid']);
+        });
     }
 
     /**
@@ -22,6 +44,6 @@ class CreateShItemsTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('sh_items');
     }
 }
