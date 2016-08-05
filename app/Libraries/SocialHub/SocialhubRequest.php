04630c4e45628dc8dc50efdc2d0462a64f1f25bd
Allen <allen@appsploration.com>
2016-08-05 11:18:39 +0800

    Added SocialhubRequest library
    Added SocialhubRequest library to handle cURL request to Socialhub and
    response from it

---
+++ app/Libraries/SocialHub/SocialhubRequest.php
@@ -0,0 +1,60 @@
+<?php namespace App\Libraries\SocialHub;
+
+use anlutro\cURL\cURL;
+use anlutro\cURL\Request;
+use anlutro\cURL\Response;
+use anlutro\cURL\cURLException;
+
+class SocialhubRequest {
+    protected static $DOMAIN = 'http://socialhub.innity-asia.com/';
+    protected static $METHOD_GET = 'get';
+    protected $apiName = '';
+    protected $curl = null;
+    
+    public function __construct() {
+        $this->curl = new cURL();
+    }
+    
+    protected function getJsonRequest() {
+        return $this->curl->newJsonRequest(
+            self::$METHOD_GET,
+            self::$DOMAIN.$this->apiName
+        )->setHeader('Accept', 'application/json')
+        ->setHeader('Content-Type', 'application/json');
+    }
+    
+    protected function send(Request $request) {
+        try {
+            $response = $request->send();
+        } catch (cURLException $e) {
+            $response = null;
+        }
+        
+        return $this->processResponse($response);
+    }
+    
+    protected function processResponse(Response $response = null)
+    {
+        $simpleResponse = [
+            'body' => '',
+            'statusText' => '',
+            'statusCode' => -1
+        ];
+
+        if (!is_null($response)) {
+            $simpleResponse['body'] = $response->body;
+            $simpleResponse['statusText'] = $response->statusText;
+            $simpleResponse['statusCode'] = $response->statusCode;
+        }
+        
+        return $simpleResponse;
+    }
+    
+    public function get() {
+        
+        $request = $this->getJsonRequest();
+        
+        return $this->send($request);
+    }
+}
+
