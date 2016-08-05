92091a1ac24f88a53178f25167787a6a91764e7c
Allen <allen@appsploration.com>
2016-08-05 11:19:55 +0800

    Added SocialhubFeedLib library
    Added child class SocialhubFeedLib to handle API to get feeds from
    SocialHub

---
+++ app/Libraries/SocialHub/SocialhubFeedLib.php
@@ -0,0 +1,7 @@
+<?php namespace App\Libraries\SocialHub;
+
+class SocialhubFeedLib extends SocialhubRequest
+{
+    protected $apiName = 'api/v2/feeds/1198946?oauth_consumer_key=gEjomHNcNFeZ4H4hA3vowrvxLytLLvqN&oauth_token=XcurrjQP6nCKqdx84B3e7nMGAcxPyMyA';
+    
+}
