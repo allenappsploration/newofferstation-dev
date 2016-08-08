2141300cbc56e1197432919caf0c8d52b5a049f9
Allen <allen@appsploration.com>
2016-08-08 11:40:55 +0800

    Updated Api/PostController@index
    Updated Api/PostController@index to return JSON data of posts

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -7,9 +7,14 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use App\Models\Post;
+
 class PostController extends Controller
 {
     public function index() {
         
+        $result = Post::with('postImg')->paginate(50);
+        
+        return $result;
     }
 }
