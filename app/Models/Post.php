b4667eb7d853c591100ec8e3d487c6738c4d60ae
Allen <allen@appsploration.com>
2016-08-08 14:55:55 +0800

    Updated Post model
    Converted start and end field to timestamp

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,6 +8,11 @@ class Post extends Model
 {
     const DATE_TYPE = ['s_n', 'wsl', 'end', 'none'];
     
+    protected $casts = [
+        'start' => 'timestamp',
+        'end' => 'timestamp'
+    ];
+    
     public function postImg()
     {
         return $this->hasMany('App\Models\PostImg', 'posts_id');
