b334b0e0e6a8caf57986eb972510ad6e66a65bc6
Allen <allen@appsploration.com>
2016-08-08 11:23:01 +0800

    Created post image migration
    Created post image migration for table creation

---
+++ database/migrations/2016_08_08_023832_create_post_imgs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostImgsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_imgs', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->integer('posts_id')->unsigned();
+            $table->float('ratio');
+            $table->String('path');
+            $table->timestamps();
+            
+            $table->foreign('posts_id')->references('id')->on('posts');
+            $table->index(['posts_id']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('post_imgs');
+    }
+}
