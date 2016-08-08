d161981b9f42c2cc2766736cf235e48beb589c24
Super Snake King <skteh@appsploration.com>
2016-08-08 17:52:43 +0800

    Remove temporary code.
    Remove temporary code.

--- app/Jobs/ConvertSHItemsToPosts.php
+++ app/Jobs/ConvertSHItemsToPosts.php
@@ -24,8 +24,6 @@ class ConvertSHItemsToPosts extends Job implements ShouldQueue
      */
     public function __construct()
     {
-        $job = (new \App\Jobs\ConvertSHItemsToPosts())->delay(30);
-        $this->dispatch($job);
         //
     }
 
