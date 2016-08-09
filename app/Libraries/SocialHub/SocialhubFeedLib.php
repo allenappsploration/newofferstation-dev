044fd208b80c2523b45de1f444c261b6f3318470
Allen <allen@appsploration.com>
2016-08-09 10:52:40 +0800

    Updated Socialhub Request apiName
    Updated Socialhub Request apiName to JSON return result

--- app/Libraries/SocialHub/SocialhubFeedLib.php
+++ app/Libraries/SocialHub/SocialhubFeedLib.php
@@ -2,6 +2,6 @@
 
 class SocialhubFeedLib extends SocialhubRequest
 {
-    protected $apiName = "api/v2/feeds/1198946?oauth_consumer_key=gEjomHNcNFeZ4H4hA3vowrvxLytLLvqN&oauth_token=XcurrjQP6nCKqdx84B3e7nMGAcxPyMyA";
+    protected $apiName = "api/v2/feeds/1198946.json?oauth_consumer_key=gEjomHNcNFeZ4H4hA3vowrvxLytLLvqN&oauth_token=XcurrjQP6nCKqdx84B3e7nMGAcxPyMyA";
     
 }
