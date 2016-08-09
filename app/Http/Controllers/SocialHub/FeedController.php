d5ced2a0d724e49b6c731dcc5a7b74b39300148a
Allen <allen@appsploration.com>
2016-08-09 11:36:28 +0800

    Updated FeedController methods
    Updated FeedController methods

--- app/Http/Controllers/SocialHub/FeedController.php
+++ app/Http/Controllers/SocialHub/FeedController.php
@@ -4,33 +4,43 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use \App\Libraries\SocialHub\SocialhubFeedLib;
 use \App\Models\SocialhubItems;
+use App\Models\SocialhubItemsPg;
 
 class FeedController extends Controller
 {
     public function index()
     {
-        $socialHubFeedLib = new SocialhubFeedLib();
-        $getSocialHubFeed = $socialHubFeedLib->get();
+        $this->storeItemsIntoDB();
 
-        $socialHubFeedArray = json_decode($getSocialHubFeed['body'], true);
+        $socialhubPagination = SocialhubItemsPg::where('is_processed', 0)->get();
 
-        if (!is_null($socialHubFeedArray)) {
-            $this->storeItemsIntoDB($socialHubFeedArray);
+        if (!empty($socialhubPagination)) {
+            $nextPageJob = (new \App\Jobs\CheckSHItemsNextPage());
+            $this->dispatch($nextPageJob);
         }
 
         $job = (new \App\Jobs\ConvertSHItemsToPosts())->delay(30);
         $this->dispatch($job);
     }
 
-    private function storeItemsIntoDB(Array $socialHubFeedArray)
+    private function storeItemsIntoDB()
     {
+        $socialHubFeedLib = new SocialhubFeedLib();
+        $getSocialHubFeed = $socialHubFeedLib->get();
+
+        if (is_null($getSocialHubFeed)) {
+            return "Something has gone wrong in the database.";
+        }
+
+        $socialHubFeedArray = json_decode($getSocialHubFeed['body'], true);
+
         $socialHubFeed = $socialHubFeedArray['items'];
 
         if (count($socialHubFeed) > 0) {
             $tags = "";
 
             for ($i = 0; $i < count($socialHubFeed); $i++) {
-                $socialHubItems = new SocialhubItems;
+                $socialHubItems = new SocialhubItems();
                 $socialHubItems->nid = $socialHubFeed[$i]['nid'];
                 $socialHubItems->tid = $socialHubFeed[$i]['brand']['tid'];
                 $socialHubItems->name = $socialHubFeed[$i]['brand']['name'];
@@ -42,14 +52,14 @@ class FeedController extends Controller
                 $socialHubItems->status = $socialHubFeed[$i]['status'];
                 $socialHubItems->created = date("Y-m-d H:i", $socialHubFeed[$i]['created']);
 
-                if (isset($socialHubFeed[$i]['images'][0]['original'])) {
+                if (!is_null($socialHubFeed[$i]['images'][0]['original'])) {
                     $socialHubItems->img = (string)$socialHubFeed[$i]['images'][0]['original']['url'];
                     $socialHubItems->img_width = $socialHubFeed[$i]['images'][0]['original']['width'];
                     $socialHubItems->img_height = $socialHubFeed[$i]['images'][0]['original']['height'];
                 }
                 $socialHubItems->extra_data = json_encode($socialHubFeed[$i]['data']);
 
-                if (count($socialHubFeed[$i]['tags']) > 0) {
+                if (!is_null($socialHubFeed[$i]['tags'])) {
                     for ($j = 0; $j < count($socialHubFeed[$i]['tags']); $j++) {
                         $tags .= (string)$socialHubFeed[$i]['tags'][$j]['name'] . ", ";
                     }
@@ -60,25 +70,21 @@ class FeedController extends Controller
                 $socialHubItems->post_language = (string)$socialHubFeed[$i]['post_language'];
 
                 if (!$socialHubItems->save()) {
-                    return false;
+                    return "Something has gone wrong while storing into database.";
                 }
             }
         }
 
-//        if (isset($socialHubFeedArray['paging'])) {
-//            $socialHubNextPage = $socialHubFeedArray['paging'];
-//
-//            if (count($socialHubNextPage) > 0) {
-//                if (count($socialHubNextPage['next']) > 0) {
-//                    // TODO: next page
-//                    $nextPageURL = str_replace_first('?oauth_consumer_key', '.json?oauth_consumer_key', $socialHubNextPage['next']);
-//                    $socialHubFeedLib = new SocialhubFeedLib();
-//                    $getSocialHubNextFeed = $socialHubFeedLib->getNextPage($nextPageURL);
-//                    $socialHubNextFeedArray = json_decode($getSocialHubNextFeed['body'], true);
-//                    $this->storeItemsIntoDB($socialHubNextFeedArray);
-//                }
-//            }
-//        }
+        if (isset($socialHubFeedArray['paging'])) {
+            
+            $socialHubNextPage = $socialHubFeedArray['paging'];
+
+            if (isset($socialHubNextPage['next'])) {
+                $socialhubPagination = new SocialhubItemsPg();
+                $socialhubPagination->next_url = $socialHubNextPage['next'];
+                $socialhubPagination->save();
+            }
+        }
     }
 
     private function str_replace_first($from, $to, $subject)
